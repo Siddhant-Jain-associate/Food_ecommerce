51bb4af70f715def4533093c55837efc81ff0434
Siddhant Jain <siddhant.jain@siddantss504.successivetech.com>
2022-09-23 12:29:13 +0530

    adding lint-staged

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -5,10 +5,10 @@ function Header() {
     <>
       <header>
         <h1>Delicious Meals Corner</h1>
-        <button type="submit" onClick={() => console.log('cart clicked...')}>Cart</button>
+        <button type="submit">Cart</button>
       </header>
       <div>
-        <img alt="Come to have a taste a varities of food" />
+        <img alt="Come to have a taste a varities of Delicious Food" />
       </div>
     </>
   );
